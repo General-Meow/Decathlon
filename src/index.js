f29f8dd143c679bed2ceca39a2e2c24ea5b0fd04
Paul Hoang <paul.hoang@external.toyotaconnected.eu>
2021-01-26 12:12:54 +0000

    format and only email when theres stock

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ sgMail.setApiKey(sendgridkey)
 
 //10kg weights: https://www.decathlon.co.uk/en/ajax/rest/model/com/decathlon/cube/commerce/inventory/InventoryActor/getStoreAvailability?storeIds=0070024000240%2C0070207902079%2C0070104001040%2C0070202002020%2C0070097400974%2C0070158801588%2C0070159101591%2C0070208402084%2C0070038700387%2C0070157401574&skuId=2524419&modelId=8491830&displayStoreDetails=false
 
-
 async function checkStockForUrl(product, url) {
   const stockResult = getJSON(url);
   const storeId = '0070024000240';
@@ -46,7 +45,7 @@ async function checkStockForUrl(product, url) {
   })
 }
 
-var minutes = 1, the_interval = minutes * 60 * 1000;
+var minutes = 60, the_interval = minutes * 60 * 1000;
 setInterval(async function () {
   console.log("I am doing my 5 minutes check");
 
@@ -59,15 +58,19 @@ setInterval(async function () {
   const weight10KgUrl = "https://www.decathlon.co.uk/en/ajax/rest/model/com/decathlon/cube/commerce/inventory/InventoryActor/getStoreAvailability?storeIds=0070024000240%2C0070207902079%2C0070104001040%2C0070202002020%2C0070097400974%2C0070158801588%2C0070159101591%2C0070208402084%2C0070038700387%2C0070157401574&skuId=969931&modelId=1042303&displayStoreDetails=false"
   const curlBarUrl = "https://www.decathlon.co.uk/en/ajax/rest/model/com/decathlon/cube/commerce/inventory/InventoryActor/getStoreAvailability?storeIds=0070024000240%2C0070207902079%2C0070104001040%2C0070202002020%2C0070097400974%2C0070158801588%2C0070159101591%2C0070208402084%2C0070038700387%2C0070157401574&skuId=2526775&modelId=8484124&displayStoreDetails=false";
 
-  result.push(await checkStockForUrl("WEIGHT TRAINING DUMBBELL KIT 20 KG", weightsStockUrl));
+  result.push(await checkStockForUrl("WEIGHT TRAINING DUMBBELL KIT 20 KG",
+      weightsStockUrl));
   result.push(await checkStockForUrl("WEIGHT TRAINING DUMBBELL KIT 10 KG",
       weights10KgSetUrl));
-  result.push(await checkStockForUrl("CAST IRON WEIGHT TRAINING DISC WEIGHT 28MM 1KG",
-      weight1KgUrl));
-  result.push(await checkStockForUrl("CAST IRON WEIGHT TRAINING DISC WEIGHT 28MM 2KG",
-      weight2KgUrl));
-  result.push(await checkStockForUrl("CAST IRON WEIGHT TRAINING DISC WEIGHT 28MM 5KG",
-      weight5KgUrl));
+  result.push(
+      await checkStockForUrl("CAST IRON WEIGHT TRAINING DISC WEIGHT 28MM 1KG",
+          weight1KgUrl));
+  result.push(
+      await checkStockForUrl("CAST IRON WEIGHT TRAINING DISC WEIGHT 28MM 2KG",
+          weight2KgUrl));
+  result.push(
+      await checkStockForUrl("CAST IRON WEIGHT TRAINING DISC WEIGHT 28MM 5KG",
+          weight5KgUrl));
   result.push(await
       checkStockForUrl("CAST IRON WEIGHT TRAINING DISC WEIGHT 28MM 10KG",
           weight10KgUrl));
@@ -75,24 +78,27 @@ setInterval(async function () {
   // console.log(result)
   var productsInStock = "";
   for (let r of result) {
-    if(r.instock)
-    productsInStock = productsInStock + "" + r.message;
+    if (r.instock) {
+      productsInStock = productsInStock + "" + r.message;
+    }
   }
 
-  const msg = {
-    to: 'spl.ivalis@gmail.com', // Change to your recipient
-    from: 'spl.ivalis@gmail.com', // Change to your verified sender
-    subject: 'Decathlon stock checker',
-    text: productsInStock,
-    // html: '<strong>some text content in html</strong>',
+  if (productsInStock.length > 0) {
+    const msg = {
+      to: 'spl.ivalis@gmail.com', // Change to your recipient
+      from: 'spl.ivalis@gmail.com', // Change to your verified sender
+      subject: 'Decathlon stock checker',
+      text: productsInStock,
+      // html: '<strong>some text content in html</strong>',
+    }
+    sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
   }
-  sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
 
 }, the_interval);
